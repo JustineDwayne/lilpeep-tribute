ddf8ca4a43c8c8171e451e2e449196093492c283
JustineDwayne <163003030+JustineDwayne@users.noreply.github.com>
2025-06-16 08:06:24 +0800

    Header component

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,27 @@
-
+import Image from 'next/image'
+import Link from 'next/link'
+import lp from 'public/logo.png'
 
 export default function Header() {
+  const links = [
+    { label: "Home", href: "/" },
+    { label: "Merch", href: "/merch" },
+    { label: "Discography", href: "/discography" },
+  ];
+
   return (
-    <div>Header</div>
-  )
+    <section className="flex justify-between items-center bg-pink-400 py-4 px-8 sticky top-0 z-40">
+      <div>
+        <Image src={lp} alt="Lil Peep Logo" className="w-32 h-auto" />
+      </div>
+
+      <nav className="flex gap-6 text-xl font-semibold  ">
+        {links.map((link, index) => (
+          <Link key={index} href={link.href} className="text-white hover:underline">
+            {link.label}
+          </Link>
+        ))}
+      </nav>
+    </section>
+  );
 }
