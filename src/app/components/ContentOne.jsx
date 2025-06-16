ddda401a53b0bcc3313946e807fba65511ba53ac
JustineDwayne <163003030+JustineDwayne@users.noreply.github.com>
2025-06-16 10:19:56 +0800

    Content Component

---
+++ src/app/components/ContentOne.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import o from "public/overlay.jpg"
+import o1 from "public/overlay1.jpg"
+import lp from "public/lilpeep1.png"
+
+
+
+export default function ContentOne() {
+  return (
+    <section className="relative min-h-screen bg-gradient-to-r from-rose-500 to-pink-900 overflow-hidden">
+    
+    </section>
+  )
+}
