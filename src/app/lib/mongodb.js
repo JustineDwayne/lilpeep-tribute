ad5ab2d4af1a16ed56c1d44c5fc2bef46004eddd
JustineDwayne <163003030+JustineDwayne@users.noreply.github.com>
2025-06-17 19:30:02 +0800

    Update mongodb.js

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,17 +1,26 @@
 // src/lib/mongodb.js
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI; // Make sure this is in your .env.local
+const uri = process.env.MONGODB_URI;
 const options = {};
 
+if (!uri) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+// Global variable to cache the connection
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
 }
-
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
 
 export default clientPromise;
